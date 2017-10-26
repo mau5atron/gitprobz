db44b9ec258de17222b398d314a10a3b9acbd920
clgoro <clgoro@gmail.com>
2017-10-26 10:33:34 -0400

    new methods file

---
+++ my_methods.rb
@@ -0,0 +1,16 @@
+def add_num(a, b)
+	return a + b
+end
+
+def fizzbuzz(num)
+	if num % 3 == 0 && num % 5 == 0
+		return "fizzbuzz"
+	elsif num % 3 == 0
+		return "fizz"
+	elsif num % 5 == 0
+		return "buzz"
+	else
+		return num
+	end
+end
+			
